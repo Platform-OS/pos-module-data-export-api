81c61704ac6e0df388be02317b1420282cd0fd01
Rafal Krysiak <rafal@near-me.com>
2024-04-29 20:11:38 +0200

    Link dotenv also

--- tests/scripts/deploy.sh
+++ tests/scripts/deploy.sh
@@ -4,11 +4,10 @@ POS_ENV="${1:-$DEFAULT_ENV}"
 POS_TEST_API_KEY=$(uuidgen)
 
 echo "Linking playwright"
-npm link @playwright/test
+npm link dotenv @playwright/test
 
 pos-cli data clean $POS_ENV --auto-confirm --include-schema
 pos-cli deploy $POS_ENV
 
-pwd
 sed -i -e "/POS_TEST_API_KEY/s/$/$POS_TEST_API_KEY/" ./tests/.env # Append the API key to the .env file
 pos-cli constants set $POS_ENV --name _data_export_api_key --value $POS_TEST_API_KEY
