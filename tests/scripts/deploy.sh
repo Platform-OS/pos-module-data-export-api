a6e2d7648c0b67c6a8c5da6eab02db5e14f64b3e
Rafal Krysiak <rafal@near-me.com>
2024-04-30 15:59:20 +0200

    Use sed to replace a test api key, not only append it to .env file - useful when developing the tests

--- tests/scripts/deploy.sh
+++ tests/scripts/deploy.sh
@@ -9,5 +9,5 @@ npm link dotenv @playwright/test
 pos-cli data clean $POS_ENV --auto-confirm --include-schema
 pos-cli deploy $POS_ENV
 
-sed -i -e "/POS_TEST_API_KEY/s/$/$POS_TEST_API_KEY/" ./tests/.env # Append the API key to the .env file
+sed -i -e "/POS_TEST_API_KEY/s/=.*/=$POS_TEST_API_KEY/" ./tests/.env # Append/replace the API key in the .env file
 pos-cli constants set $POS_ENV --name _data_export_api_key --value $POS_TEST_API_KEY
